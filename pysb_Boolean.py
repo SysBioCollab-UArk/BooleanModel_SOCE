da3684e260ab3e3e14fc43e5cdea5b0d06298580
lh64 <lh64@cornell.edu>
2023-01-11 17:44:48 -0600

    Added some preliminary code for using the BngFileInterface to speed up SSA simulations

--- pysb_Boolean.py
+++ pysb_Boolean.py
@@ -2,10 +2,9 @@ from pysb.importers.boolean import model_from_boolean
 from pysb.simulator import BngSimulator
 import numpy as np
 import matplotlib.pyplot as plt
+from pysb.bng import BngFileInterface
 
 model = model_from_boolean('mapk_soce.txt', mode='GSP')
-
-
 
 # print(model)
 # print (model.monomers)
@@ -39,34 +38,44 @@ traj={}
 
 for obs in observables:
     traj[obs]=[None]*n_runs
-sim=BngSimulator(model,verbose=False)
-for n in range(n_runs):
-    print(n)
-    #Step 1 equilibration
-    tspan1 = np.linspace(0, 10, 101)
-    output=sim.run(tspan=tspan1)
-    # for sp in model.species:
-    #     print(sp)
-    # quit()
-    for obs in observables:
-        traj[obs][n]=output.observables[obs] # here we need the nth simulation result for each observable
 
-    #step2 Adding Brafi
-    tspan2=np.linspace(0, 10, 101)
-    initials=output.species[-1]
-    initials[0]=0
-    initials[1]=1
-    output2=sim.run(tspan=tspan2,initials=initials)
-    for obs in observables:
-        traj[obs][n]=np.append(traj[obs][n],output2.observables[obs])
+# sim = BngSimulator(model, verbose=True)
+
+with BngFileInterface(model) as bng:
+    bng.action('generate_network')
+
+    for n in range(n_runs):
+        print(n)
+        # Step 1 equilibration
+        # tspan1 = np.linspace(0, 10, 101)
+        # output = sim.run(tspan=tspan1)
+        bng.action('simulate', method='ssa', t_end=10, n_steps=100)
+        bng.execute()
+        output = bng.read_simulation_results()
+        # TODO: still need to figure out how to use the BngFileInterface correctly
+        # for sp in model.species:
+        #     print(sp)
+        # quit()
+        for obs in observables:
+            traj[obs][n]=output.observables[obs] # here we need the nth simulation result for each observable
+
+        #step2 Adding Brafi
+        tspan2=np.linspace(0, 10, 101)
+        initials=output.species[-1]
+        initials[0]=0
+        initials[1]=1
+        output2 = sim.run(tspan=tspan2,initials=initials)
+        for obs in observables:
+            traj[obs][n]=np.append(traj[obs][n],output2.observables[obs])
 
 
-    # Step 3, remove external calcium AND add pump inhibitor
+        # Step 3, remove external calcium AND add pump inhibitor
 
 
-    # Step 4, add external calcium
+        # Step 4, add external calcium
 
-    # Step 5, remove pump inhibitor
+        # Step 5, remove pump inhibitor
+
 avg_traj={}
 for obs in observables:
     avg_traj[obs]=np.mean(np.array(traj[obs]),axis=0)
