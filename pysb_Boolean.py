69bff7eaf0e13284a43e51b66defa2672ed5ce46
lh64 <lh64@cornell.edu>
2023-10-19 06:24:18 -0500

    Refactored pysb_Boolean.py to provide functions for running stochastic simulations of the Boolean model in GSP and ROA modes. Also created a run_python_Boolean.py file to demonstrate how to use the functions from pysb_Boolean.py

--- pysb_Boolean.py
+++ pysb_Boolean.py
@@ -2,115 +2,160 @@ from pysb.importers.boolean import model_from_boolean
 from pysb.simulator import BngSimulator
 import numpy as np
 import matplotlib.pyplot as plt
-from pysb.bng import *
-from pysb.generator.bng import BngPrinter
-from pysb.bng import BngFileInterface
 
-mode = 'GSP'  # 'ROA'
-model = model_from_boolean('mapk_soce.txt', mode=mode)
 
-n_runs = 1000
+class SimStep(object):
+    def __init__(self, label, delta_t, condition=None):
+        self.label = label
+        self.delta_t = delta_t
+        self.condition = condition
 
-species_to_plot = ['Ca_cyt', 'BRAF', 'Ca_channel', 'Ca_ER', 'Ca_ext', 'Ca_pump_ER', 'ERK', 'Gene_exp', 'MEK']
-observables = [
-    ['Ca_cyt_1_True_obs', 'Ca_cyt_2_True_obs', 'Ca_cyt_3_True_obs'],
-    ['BRAF_True_obs'],
-    ['Ca_channel_True_obs'],
-    ['Ca_ER_True_obs'],
-    ['Ca_ext_True_obs'],
-    ['Ca_pump_ER_True_obs'],
-    ['ERK_True_obs'],
-    ['Gene_exp_True_obs'],
-    ['MEK_True_obs']
-]
-markers = ['s', 'o', '^', '*', 'd', 'H', 'v', '<', '>']
-colors = ['blue', 'green', 'black', 'red', 'purple', 'brown', 'yellow', 'orange', 'cyan']
 
-# tspan1 = np.linspace(0, 10, 101)
-# sim = BngSimulator(model, verbose=True)
-# out = sim.run(tspan=tspan1, n_runs=2)
-# print(np.array(out.observables).shape)
-# quit()
+def sim_protocol(model, sim_steps, n_runs=1, t_start=0, mode='GSP', verbose=False):
+    sim = BngSimulator(model, verbose=verbose)
+    tspans = []
+    outputs = []
+    additional_args = {}
+    end_string = ""
+    sp_names = None
+    if mode == 'GSP' or mode == 'GA':
+        end_string = ")"
+    elif mode == 'ROA':
+        end_string = ", reset='N')"
+        # The output_step_interval is the number of nodes with update rules, multiplied by 2. That includes the model
+        # monomers (not necessarily all of them) PLUS the RESET monomer that's created automatically.
+        additional_args['output_step_interval'] = (int(model.parameters['N_NODES'].value) + 1) * 2
+    else:
+        print("Mode '%s' not recognized. Quitting.")
+        quit(1)
+    for step, ss in enumerate(sim_steps):
+        print("step %d: %s" % (step, ss.label))
+        # first step
+        if step == 0:
+            initials = None
+            if ss.condition is not None:
+                initials = {}
+                for c in ss.condition:
+                    if c is not None:
+                        initials["%s(state='%s'%s" % (c[0], str(c[1]), end_string)] = 1
+                        initials["%s(state='%s'%s" % (c[0], str(not c[1]), end_string)] = 0
+        # subsequent steps
+        else:
+            t_start = tspans[-1][-1]
+            initials = [sp[-1] for sp in outputs[-1].species]
+            n_runs = 1
+            if ss.condition is not None:
+                for c in ss.condition:
+                    idx1 = sp_names.index("%s(state='%s'%s" % (c[0], str(c[1]), end_string))
+                    idx2 = sp_names.index("%s(state='%s'%s" % (c[0], str(not c[1]), end_string))
+                    for init in initials:
+                        init[idx1] = 1
+                        init[idx2] = 0
+        # run simulations
+        if mode == 'GSP' or mode == 'GA':
+            tspans.append(np.linspace(t_start, t_start + ss.delta_t, int(ss.delta_t * 10) + 1))
+            time = tspans[-1]
+        elif mode == 'ROA':
+            additional_args['max_sim_steps'] = additional_args['output_step_interval'] * ss.delta_t
+            tspans.append(t_start + np.arange(ss.delta_t + 1))
+            time = [0, additional_args['max_sim_steps'] * 1000]
+        outputs.append(sim.run(tspan=time, initials=initials, n_runs=n_runs, cleanup=True, **additional_args))
+        # store species names as strings if this is the first simulation
+        if sp_names is None:
+            sp_names = [str(m) for m in model.species]
 
-# time intervals for each step
-tend_1 = 10  # step1: equilibration
-tend_2 = 50  # step2: add BRAFi
-tend_3 = 10  # step3: remove external calcium and add pump inhibitor
-tend_4 = 50  # step4: add external calcium
-tend_5 = 50  # step5: remove pump inhibitor
+    return tspans, outputs
 
-with BngFileInterface(model, verbose=True) as bng:
-    bng.action('generate_network')
-    for n in range(n_runs):
-        # print(n)
-        # step1: equilibration
-        tspan1 = np.linspace(0, tend_1, tend_1+1) # with 11 points
-        tspan_all = tspan1
-        bng.action('simulate', method='ssa', t_start=tspan_all[0], t_end=tspan_all[-1],
-                   n_steps=len(tspan1)-1, suffix=str(n))
 
-        # step2: add BRAFi
-        tspan2 = np.linspace(0, tend_2, tend_2+1) # with 51 points
-        bng.set_concentration("BRAFi(state~False)", 0)
-        bng.set_concentration("BRAFi(state~True)", 1)
-        kwarg = {'continue': 1}
-        bng.action('simulate', method='ssa', t_start=tspan_all[-1], t_end=tspan_all[-1]+tspan2[-1],
-                   n_steps=len(tspan2)-1, suffix=str(n), **kwarg)
-        tspan_all = np.append(tspan_all, tspan2[1:] + tspan1[-1])
+class ObsToPlot(object):
+    def __init__(self, name, label, color=None, marker=None):
+        if isinstance(name, str):
+            name = [name]
+        self.name = name
+        self.label = label
+        self.color = color
+        self.marker = marker
 
-        # step3: remove external calcium and add pump inhibitor
-        tspan3 = np.linspace(0, tend_3, tend_3+1)
-        bng.set_concentration("Ca_ext(state~False)", 1)
-        bng.set_concentration("Ca_ext(state~True)", 0)
-        bng.set_concentration("pumpi(state~False)", 0)
-        bng.set_concentration("pumpi(state~True)", 1)
-        bng.action('simulate', method='ssa', t_start=tspan_all[-1], t_end=tspan_all[-1]+tspan3[-1],
-                   n_steps=len(tspan3)-1, suffix=str(n), **kwarg)
-        tspan_all = np.append(tspan_all, tspan3[1:] + tspan1[-1] + tspan2[-1])
 
-        # step4: add external calcium
-        tspan4 = np.linspace(0, tend_4, tend_4+1)
-        bng.set_concentration("Ca_ext(state~False)", 0)
-        bng.set_concentration("Ca_ext(state~True)", 1)
-        bng.action('simulate', method='ssa', t_start=tspan_all[-1], t_end=tspan_all[-1]+tspan4[-1],
-                   n_steps=len(tspan4)-1, suffix=str(n), **kwarg)
-        tspan_all = np.append(tspan_all, tspan4[1:] + tspan1[-1] + tspan2[-1] + tspan3[-1])
+def plot_results(tspans, outputs, observables, mode, multi_plots=False, show_plots=False):
+    if not multi_plots:
+        plt.figure(figsize=(12.8, 4.8))
+        plt.ylim(bottom=-0.05, top=1.05)
+    lines = []
+    labels = []
+    for obs in observables:
+        if multi_plots:
+            plt.figure(figsize=(12.8, 4.8))
+            plt.ylim(bottom=-0.05, top=1.05)
+            lines = []
+            labels = []
+        labels.append(obs.label)
+        for step, out, tspan in zip(range(len(outputs)), outputs, tspans):
+            out_observables = np.array(out.observables)
+            y = np.mean(out_observables[obs.name[0]], axis=0)
+            if len(obs.name) > 1:
+                for i in range(1, len(obs.name)):
+                    y += np.mean(out_observables[obs.name[i]], axis=0)
+                y /= len(obs.name)
+            line, = plt.plot(tspan, y, lw=3, color=obs.color)
+            if step == 0:
+                lines.append(line)
+            # plot vertical lines for each step
+            bottom, top = plt.ylim()
+            plt.plot([tspan[-1], tspan[-1]], [bottom, top], 'k--')
+    plt.xlabel('round' if mode == 'ROA' else 'time', fontsize=16)
+    plt.ylabel('value', fontsize=16)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    plt.legend(loc=0, handles=lines, labels=labels, bbox_to_anchor=(1, 1), fontsize=15)
+    plt.tight_layout()
+    if show_plots:
+        plt.show()
 
-        # step5: remove pump inhibitor
-        tspan5 = np.linspace(0, tend_5, tend_5+1)
-        bng.set_concentration("pumpi(state~False)", 1)
-        bng.set_concentration("pumpi(state~True)", 0)
-        bng.action('simulate', method='ssa', t_start=tspan_all[-1], t_end=tspan_all[-1]+tspan5[-1],
-                   n_steps=len(tspan5)-1, suffix=str(n), **kwarg)
-        tspan_all = np.append(tspan_all, tspan5[1:] + tspan1[-1] + tspan2[-1] + tspan3[-1] + tspan4[-1])
 
-        # reset concentration
-        bng.action('resetConcentrations')
+if __name__ == '__main__':
 
-    # run the simulation and plot the results
-    bng.execute()
-    result = BngFileInterface.read_simulation_results_multi(['%s_%d' % (bng.base_filename, n) for n in range(n_runs)])
-    result = np.array(result)
-    plt.figure(figsize=(12.8, 4.8))
-    for sp, obs, marker, color in zip(species_to_plot, observables, markers, colors):
-        print(sp)
-        avg_traj = np.mean(result[:][obs[0]], axis=0)
-        # n_obs = len(obs)
-        if len(obs) > 1:
-            for i in range(1, len(obs)):
-                avg_traj += np.mean(result[:][obs[i]], axis=0)
-            avg_traj /= len(obs)
-        # for n in range(n_runs):
-        #     plt.plot(tspan_all, result[n][obs[0]], lw=1, color=obs[1])
-        plt.plot(tspan_all[len(tspan1):]-tspan1[-1], avg_traj[len(tspan1):], marker=marker, color=color, label=sp)
+    mode = 'GSP'  # 'GSP', 'GA', 'ROA'
+    model = model_from_boolean('mapk_soce.txt', mode=mode)
+    n_runs = 100
 
-plt.legend(loc=0, fontsize=15)
-plt.xlim(xmin=0)
-plt.ylim((-0.1, 1.1))
-plt.xlabel('time', fontsize=16)
-plt.ylabel('probability ON', fontsize=16)
-plt.xticks(fontsize=16)
-plt.yticks(fontsize=16)
-plt.tight_layout()
+    step_labels = [
+        "equilibration",
+        "add BRAFi",
+        "remove external calcium and add pump inhibitor",
+        "add external calcium"  # ,
+        # "remove pump inhibitor"
+    ]
+    delta_ts = [40, 10, 10, 100, 100]
+    conditions = [
+        None,
+        [("BRAFi", True)],
+        [("Ca_ext", False), ("pumpi", True)],
+        [("Ca_ext", True)]  # ,
+        # [("pumpi", False)]
+    ]
 
-plt.show()
+    sim_steps = [SimStep(label, delta_t, condition) for label, delta_t, condition
+                 in zip(step_labels, delta_ts, conditions)]
+
+    tspans, outputs = sim_protocol(model, sim_steps, n_runs=n_runs, t_start=-sim_steps[0].delta_t, mode=mode,
+                                   verbose=True)
+
+    obs_names = [
+        ['Ca_cyt_1_True_obs', 'Ca_cyt_2_True_obs', 'Ca_cyt_3_True_obs'],
+        'BRAF_True_obs',
+        'Ca_channel_True_obs',
+        'Ca_ER_True_obs',
+        'Ca_ext_True_obs',
+        'Ca_pump_ER_True_obs',
+        'ERK_True_obs',
+        'Gene_exp_True_obs',
+        'MEK_True_obs'
+    ]
+    obs_labels = ['Ca_cyt', 'BRAF', 'Ca_channel', 'Ca_ER', 'Ca_ext', 'Ca_pump_ER', 'ERK', 'Gene_exp', 'MEK']
+    obs_colors = ['blue', 'green', 'black', 'red', 'purple', 'brown', 'yellow', 'orange', 'cyan']
+    # obs_markers = ['s', 'o', '^', '*', 'd', 'H', 'v', '<', '>']
+
+    observables = [ObsToPlot(name, label, color) for name, label, color in zip(obs_names, obs_labels, obs_colors)]
+
+    plot_results(tspans, outputs, observables, mode, multi_plots=False, show_plots=True)
